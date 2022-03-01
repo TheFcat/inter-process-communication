8eb36a0ec3d1e844bd301ffd47b0c11f8c0eb695
TheFcat <ken199091@gmail.com>
2022-03-01 21:49:18 +0800

    implement socket_client

--- server.py
+++ server.py
@@ -1,14 +1,39 @@
 import re
+import socket
+import time
+import subprocess
+
+from socket_client import HOST, PORT
 
 
-input_data = input("Server is ready. You can type intergers and then click [ENTER].  Clients will show the mean, median, and mode of the input values.\n")
-data = input_data.strip().split()
-parsed_data = []
-pattern = '^\d+$'
-for string_input in data:
-    if not re.fullmatch(pattern, string_input):
-        break
-    parsed_data.append(int(string_input))
+def get_parsed_data(data):
+    result = []
+    pattern = '^\d+$'
+    for string_input in data:
+        if not re.fullmatch(pattern, string_input):
+            break
+        result.append(int(string_input))
 
-if len(data) != len(parsed_data):
-    print('please input integer only')
+    if len(data) != len(result):
+        print('please input integer only')
+    return result
+
+
+if __name__ == '__main__':
+    input_data = input(
+        "Server is ready. You can type intergers and then click [ENTER].  Clients will show the mean, median, and mode of the input values.\n")
+
+    inputs = input_data.strip().split()
+
+    parsed_data = get_parsed_data(inputs)
+
+    client1 = subprocess.Popen(['python', 'socket_client.py'])
+
+    time.sleep(1)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    s.send(str(parsed_data).encode())
+    s.recv(1024)
+    s.close()
--- socket_client.py
+++ socket_client.py
@@ -1,4 +1,29 @@
+import socket
+HOST = '127.0.0.1'
+PORT = 7000
 
 
 class SocketClient:
-    pass
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((HOST, PORT))
+        self.socket.listen(5)
+        print('Client1 is ready')
+        self.run()
+
+    def run(self):
+        conn, addr = self.socket.accept()
+        input_data = conn.recv(1024)
+        self.calculate_mean(eval(input_data.decode()))
+        conn.send('done'.encode())
+        conn.close()
+
+    def calculate_mean(self, input_data):
+        if not input_data:
+            return
+        mean = sum(input_data) / len(input_data)
+        print(f'Mean is {mean}')
+
+
+if __name__ == '__main__':
+    SocketClient()
