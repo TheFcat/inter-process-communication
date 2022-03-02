2a472cf37563d9ba742223e506455e615adee21a
TheFcat <ken199091@gmail.com>
2022-03-02 20:31:09 +0800

    implement shared_memory_client

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import re
 import socket
-import time
 import subprocess
-
+import time
+from multiprocessing import shared_memory
 from socket_client import HOST, PORT
 
 
@@ -20,20 +20,24 @@ def get_parsed_data(data):
 
 
 if __name__ == '__main__':
+    shm = shared_memory.SharedMemory(create=True, size=5000)
+    client1 = subprocess.Popen(['python', 'socket_client.py'])
+    # client2 = subprocess.Popen(['python', 'pipe_client.py'])
+    client3 = subprocess.Popen(['python', 'shared_memory_client.py', shm.name])
     input_data = input(
         "Server is ready. You can type intergers and then click [ENTER].  Clients will show the mean, median, and mode of the input values.\n")
 
     inputs = input_data.strip().split()
-
     parsed_data = get_parsed_data(inputs)
-
-    client1 = subprocess.Popen(['python', 'socket_client.py'])
-
-    time.sleep(1)
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
 
     s.send(str(parsed_data).encode())
     s.recv(1024)
+    buffer = shm.buf
+    buffer[1:len(parsed_data) + 1] = bytearray(parsed_data)
+    buffer[0] = len(parsed_data)
+    time.sleep(5)
     s.close()
+    shm.close()
+    shm.unlink()
--- shared_memory_client.py
+++ shared_memory_client.py
@@ -1,4 +1,23 @@
+import sys
+import array
+import time
+from multiprocessing import shared_memory
+from collections import Counter
 
 
-class SharedMemoryClient:
-    pass
+print('Client3 is ready')
+
+shm = shared_memory.SharedMemory(name=sys.argv[-1].strip())
+while shm.buf[0] == 0:
+    time.sleep(1)
+count_mapping = Counter(list(array.array('b', shm.buf[1:shm.buf[0] + 1])))
+mode = []
+most_common = None
+for value, count in count_mapping.most_common():
+    if most_common is not None and most_common != count:
+        break
+    most_common = count
+    mode.append(str(value))
+
+print(f"Mode is	{','.join(mode)}")
+shm.close()
